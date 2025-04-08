de5d7c97cedb18f0ec83637923d1c1cf609367ca
ad-ng <adolphengoga@gmail.com>
2025-04-08 07:56:28 +0200

    finishing get current user api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/gurds/auth.guards';
+import { Request } from 'express';
 
+@UseGuards(AuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('')
+  currentUser(@Req() req: Request) {
+    return this.userService.getCurrentUser(req.user);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+  async getCurrentUser(user) {
+    const userId: number = user.id;
+    const currentUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!currentUser) throw new UnauthorizedException();
+    return {
+      message: 'user found successfully',
+      data: currentUser,
+    };
+  }
 }
