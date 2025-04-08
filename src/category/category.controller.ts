4ceea605ea4431966260db5c368c1778be3598f3
ad-ng <adolphengoga@gmail.com>
2025-04-08 10:22:32 +0200

    adding get all categories

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/gurds/auth.guards';
+import { CategoryService } from './category.service';
 
+@UseGuards(AuthGuard)
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+  @Get()
+  allCategories() {
+    return this.categoryService.getAllCategories();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllCategories() {
+    const allCats = await this.prisma.category.findMany();
+    return {
+      message: 'Categories found successfully',
+      data: allCats,
+    };
+  }
+}
