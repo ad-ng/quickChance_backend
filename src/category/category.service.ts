e9841cd04ef02d802d1f3def82cb8c09312cebd1
ad-ng <adolphengoga@gmail.com>
2025-04-08 11:37:54 +0200

    adding create category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CategoryDTO } from './dtos';
 
@@ -14,7 +14,20 @@ export class CategoryService {
     };
   }
 
-  addCategory(dto: CategoryDTO) {
-    return 'category added';
+  async addCategory(dto: CategoryDTO) {
+    const checkIfNameExist = await this.prisma.category.findUnique({
+      where: { name: dto.name },
+    });
+
+    if (checkIfNameExist) {
+      throw new BadRequestException('category already exist');
+    }
+
+    const addedCategory = await this.prisma.category.create({ data: dto });
+
+    return {
+      message: 'Category Created Successfully',
+      data: addedCategory,
+    };
   }
 }
