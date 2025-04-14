99395307c37de99b23a57617df59dd8097e7243d
ad-ng <adolphengoga@gmail.com>
2025-04-14 07:37:52 +0200

    checking if i like opp

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Request } from 'express';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('like')
@@ -11,5 +12,10 @@ export class LikeController {
   @Get(':oppId')
   singleOppAllLikes(@Param() param: any) {
     return this.likeService.singleOpp(param);
+  }
+
+  @Get('check/:oppId')
+  checkIfIliked(@Param() param: any, @Req() req: Request) {
+    return this.likeService.checkIfIlikedOpp(param, req.user);
   }
 }
--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -14,6 +18,51 @@ export class LikeService {
     return {
       message: 'likes fetched successfully',
       data: { TotalLikes: countOppLikes },
+    };
+  }
+
+  async checkIfIlikedOpp(params, user) {
+    const userId: number = parseInt(user.id, 10);
+    const checkUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!checkUser) {
+      throw new UnauthorizedException();
+    }
+
+    const oppId: number = parseInt(params.oppId, 10);
+    const checkOpp = await this.prisma.opportunity.findUnique({
+      where: { id: oppId },
+    });
+
+    if (!checkOpp) {
+      throw new NotFoundException();
+    }
+
+    const checkLike = await this.prisma.like.findUnique({
+      where: {
+        oppId_userid: {
+          oppId: oppId,
+          userid: userId,
+        },
+      },
+    });
+
+    if (!checkLike) {
+      return {
+        message: 'Check like found',
+        data: {
+          checkLike: false,
+        },
+      };
+    }
+
+    return {
+      message: 'Check like found',
+      data: {
+        checkLike: true,
+      },
     };
   }
 }
