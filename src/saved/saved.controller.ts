647ef63c973db97e46a08b7505e202819db9354a
ad-ng <adolphengoga@gmail.com>
2025-04-14 08:37:49 +0200

    total opp saves

--- src/saved/saved.controller.ts
+++ src/saved/saved.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SavedService } from './saved.service';
 
 @Controller('saved')
-export class SavedController {}
+export class SavedController {
+  constructor(private savedService: SavedService) {}
+
+  @Get(':oppId')
+  total(@Param() param: any) {
+    return this.savedService.getTotalOfSave(param);
+  }
+}
--- src/saved/saved.service.ts
+++ src/saved/saved.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SavedService {}
+export class SavedService {
+  constructor(private prisma: PrismaService) {}
+
+  async getTotalOfSave(params) {
+    const oppId: number = parseInt(params.oppId, 10);
+    const checkOpp = await this.prisma.opportunity.findUnique({
+      where: { id: oppId },
+    });
+
+    if (!checkOpp) throw new NotFoundException();
+
+    const totalSavedOpps: number = await this.prisma.saved.count({
+      where: { oppId: oppId },
+    });
+
+    return {
+      message: 'fetching total of all saved Opps',
+      data: {
+        total: totalSavedOpps,
+      },
+    };
+  }
+}
