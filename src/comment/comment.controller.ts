83124d8baa198a6f2942e36285a21641f97fa714
ad-ng <adolphengoga@gmail.com>
2025-04-15 14:33:04 +0200

    posting a comment

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard, RolesGuard } from 'src/auth/guards';
 import { CommentService } from './comment.service';
+import { Request } from 'express';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('comment')
@@ -15,5 +16,10 @@ export class CommentController {
   @Get(':oppId')
   getComments(@Param() param: any) {
     return this.commentService.getAllCommentsOnOpp(param);
+  }
+
+  @Post(':oppId')
+  postComment(@Req() req: Request, @Param() param: any) {
+    return this.commentService.addComment(param, req.user);
   }
 }
