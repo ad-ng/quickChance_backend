695d8d361a20582a25b9c362f160f38c180d90e8
ad-ng <adolphengoga@gmail.com>
2025-05-20 08:44:50 +0200

    initialising notification feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AppController } from './app.controller';
 import { LikeModule } from './like/like.module';
 import { SavedModule } from './saved/saved.module';
 import { CommentModule } from './comment/comment.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { CommentModule } from './comment/comment.module';
     LikeModule,
     SavedModule,
     CommentModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
 })
