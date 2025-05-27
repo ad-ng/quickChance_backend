3f60d26b9ba53bb22c328ec428697aae6592d4ca
ad-ng <adolphengoga@gmail.com>
2025-05-27 07:21:28 +0200

    fixng notification

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,93 +1,102 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// import {
-//   Injectable,
-//   InternalServerErrorException,
-//   NotFoundException,
-//   UnauthorizedException,
-// } from '@nestjs/common';
-// import { PrismaService } from 'src/prisma/prisma.service';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-// @Injectable()
-// export class NotificationsService {
-//   constructor(private prisma: PrismaService) {}
+@Injectable()
+export class NotificationsService {
+  constructor(private prisma: PrismaService) {}
 
-//   async fetchAllNotifications() {
-//     try {
-//       const allNotifications = await this.prisma.notification.findMany();
-//       return {
-//         message: 'notifications fetched successfully',
-//         data: allNotifications,
-//       };
-//     } catch (error) {
-//       throw new InternalServerErrorException(error);
-//     }
-//   }
+  async fetchAllNotifications(user) {
+    const userId: number = user.id;
+    const checkUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
 
-//   async countAllNot(user) {
-//     const userId: number = user.id;
-//     const checkUser = await this.prisma.user.findUnique({
-//       where: { id: userId },
-//     });
-//     if (!checkUser) throw new UnauthorizedException();
+    if (!checkUser) throw new UnauthorizedException();
 
-//     const notificationCount = await this.prisma.notification.count({
-//       where: { userId },
-//     });
-//     return {
-//       message: 'notifications count fetched successfully',
-//       data: {
-//         notificationCount,
-//       },
-//     };
-//   }
-//   catch(error) {
-//     throw new InternalServerErrorException(error);
-//   }
+    try {
+      const allNotifications = await this.prisma.userNotification.findMany({
+        where: { userId },
+      });
+      return {
+        message: 'notifications fetched successfully',
+        data: allNotifications,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 
-//   async readingNotification(params, req) {
-//     const userId: number = req.id;
-//     const notId = parseInt(params['id'], 10);
-//     const checkNot = await this.prisma.notification.findUnique({
-//       where: { id: notId, userId },
-//     });
-//     if (!checkNot) throw new NotFoundException();
+  async countAllNot(user) {
+    const userId: number = user.id;
+    const checkUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!checkUser) throw new UnauthorizedException();
 
-//     try {
-//       await this.prisma.notification.update({
-//         where: { id: notId },
-//         data: { isRead: true },
-//       });
-//       return {
-//         message: 'isRead updated successfully',
-//       };
-//     } catch (error) {
-//       throw new InternalServerErrorException(error);
-//     }
-//   }
+    const notificationCount = await this.prisma.userNotification.count({
+      where: { userId },
+    });
+    return {
+      message: 'notifications count fetched successfully',
+      data: {
+        notificationCount,
+      },
+    };
+  }
+  catch(error) {
+    throw new InternalServerErrorException(error);
+  }
 
-//   async deleteNotification(param, req) {
-//     const notId: number = parseInt(param['id'], 10);
-//     const userId: number = req.id;
+  async readingNotification(params, req) {
+    const userId: number = req.id;
+    const notId = parseInt(params['id'], 10);
+    const checkNot = await this.prisma.userNotification.findUnique({
+      where: { id: notId, userId },
+    });
+    if (!checkNot) throw new NotFoundException();
 
-//     const checKNot = await this.prisma.notification.findUnique({
-//       where: { id: notId },
-//     });
+    try {
+      await this.prisma.userNotification.update({
+        where: { id: notId },
+        data: { isRead: true },
+      });
+      return {
+        message: 'isRead updated successfully',
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 
-//     if (!checKNot) throw new NotFoundException();
+  async deleteNotification(param, req) {
+    const notId: number = parseInt(param['id'], 10);
+    const userId: number = req.id;
 
-//     const checkUser = await this.prisma.user.findUnique({
-//       where: { id: userId },
-//     });
+    const checKNot = await this.prisma.userNotification.findUnique({
+      where: { id: notId },
+    });
 
-//     if (!checkUser) throw new UnauthorizedException();
+    if (!checKNot) throw new NotFoundException();
 
-//     try {
-//       await this.prisma.notification.delete({ where: { id: notId } });
-//       return {
-//         message: 'notification deleted successfully',
-//       };
-//     } catch (error) {
-//       throw new InternalServerErrorException(error);
-//     }
-//   }
-// }
+    const checkUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!checkUser) throw new UnauthorizedException();
+
+    try {
+      await this.prisma.notification.delete({ where: { id: notId } });
+      return {
+        message: 'notification deleted successfully',
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
